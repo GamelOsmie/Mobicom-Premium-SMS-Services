d7f004d1bc0fbaae976ae5ff446d3b9fdc720901
GamelOsmie <gamel_solutions@hotmail.com>
2023-12-04 16:54:48 +0000

    encryption added

--- server/src/utils/cryptoConfig.js
+++ server/src/utils/cryptoConfig.js
@@ -1,20 +1,20 @@
 const crypto = require('crypto');
 
-// // Encryption using the 'crypto' module
-// const encryption = (text) => {
-//   const cipher = crypto.createCipher('aes-256-cbc', process.env.JWT_SECRET); // Using AES-256-CBC algorithm
-//   let encrypted = cipher.update(text, 'utf8', 'hex');
-//   encrypted += cipher.final('hex');
-//   return encrypted;
-// };
+// Encryption using the 'crypto' module
+const encryption = (text) => {
+  const cipher = crypto.createCipher('aes-256-cbc', process.env.JWT_SECRET); // Using AES-256-CBC algorithm
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return encrypted;
+};
 
-// // Decryption using the 'crypto' module
-// const decryption = (encryptedText) => {
-//   const decipher = crypto.createDecipher('aes-256-cbc', process.env.JWT_SECRET);
-//   let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
-//   decrypted += decipher.final('utf8');
-//   return decrypted;
-// };
+// Decryption using the 'crypto' module
+const decryption = (encryptedText) => {
+  const decipher = crypto.createDecipher('aes-256-cbc', process.env.JWT_SECRET);
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+};
 
 // // Encryption function
 // const encryption = (plainText) => {
